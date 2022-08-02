605c652574df10580cbb8ae922d3b5c466e07a83
abdullah <Abdullahamzeh@hotmail.com>
2022-08-02 13:14:08 -0500

    lab8

--- lab7/src/Login.js
+++ lab7/src/Login.js
@@ -1,17 +1,49 @@
-function Login(){
-    return (
-        <div>
-            Sign In Form
-            <br/>
-            username:
-            <input type="text"></input>
-            <br/>
-            password:
-            <input type="password"></input>
-            <br/>
-            <input type="button" value="Login"></input>
-        </div>
-    )
+import axios from "axios";
+import { useState } from "react";
+
+function Login() {
+  let [state, setter] = useState({ username: "", password: "" });
+
+  function login() {
+    axios
+      .post("http://localhost:8080/api/v1/uaa/signin", {
+        email: state.username,
+        password: state.password,
+      })
+      .then((e) => {
+        console.log(e);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+  function onValChange(val) {
+    let copy = { ...state };
+    if (val.target.name == "username") copy.username = val.target.value;
+    else copy.password = val.target.value;
+    setter(copy);
+  }
+  return (
+    <div>
+      Sign In Form
+      <br />
+      username:
+      <input
+        type="text"
+        name="username"
+        onChange={(e) => onValChange(e)}
+      ></input>
+      <br />
+      password:
+      <input
+        type="password"
+        name="password"
+        onChange={(e) => onValChange(e)}
+      ></input>
+      <br />
+      <input type="button" value="Login" onClick={login}></input>
+    </div>
+  );
 }
 
 export default Login;
--- lab7/src/Signup.js
+++ lab7/src/Signup.js
@@ -1,23 +1,85 @@
-function Signup(){
-    return (
-        <div>
-            Sign Up Form
-            <br/>
-            firstname:
-            <input type="text"></input>
-            <br/>
-            lastname:
-            <input type="text"></input>
-            <br/>
-            username:
-            <input type="text"></input>
-            <br/>
-            password:
-            <input type="password"></input>
-            <br/>
-            <input type="button" value="Signup"></input>
-        </div>
-    )
+import axios from "axios";
+import { useState } from "react";
+
+function Signup() {
+  let [state, setter] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+  });
+  function onValChange(val) {
+    let copy = { ...state };
+    switch (val.target.name) {
+      case "firstname":
+        copy.firstname = val.target.value;
+        break;
+      case "lastname":
+        copy.lastname = val.target.value;
+        break;
+      case "username":
+        copy.username = val.target.value;
+        break;
+      case "password":
+        copy.password = val.target.value;
+        break;
+
+      default:
+        break;
+    }
+    setter(copy);
+  }
+  function signup() {
+    axios
+      .post("http://localhost:8080/api/v1/uaa/signup", {
+        firstname: state.firstname,
+        lastname: state.lastname,
+        email: state.username,
+        password: state.password,
+      })
+      .then((e) => {
+        console.log(e);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  return (
+    <div>
+      Sign Up Form
+      <br />
+      firstname:
+      <input
+        type="text"
+        name="firstname"
+        onChange={(e) => onValChange(e)}
+      ></input>
+      <br />
+      lastname:
+      <input
+        type="text"
+        name="lastname"
+        onChange={(e) => onValChange(e)}
+      ></input>
+      <br />
+      username:
+      <input
+        type="text"
+        name="username"
+        onChange={(e) => onValChange(e)}
+      ></input>
+      <br />
+      password:
+      <input
+        type="password"
+        name="password"
+        onChange={(e) => onValChange(e)}
+      ></input>
+      <br />
+      <input type="button" value="Signup" onClick={signup}></input>
+    </div>
+  );
 }
 
 export default Signup;
