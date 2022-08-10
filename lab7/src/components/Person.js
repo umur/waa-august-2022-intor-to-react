cf5b788a54126f9d5ca01075a2b044d07506f5cf
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2022-08-10 12:16:13 -0500

    intro to react lab7 is done

---
+++ lab7/src/components/Person.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+let Person = () => {
+
+    let [personState, setPersonState] = useState({
+        fname: 'default fname',
+        lname: 'default lname'
+    })
+
+    const onChanged = (event) => {
+        setPersonState({...personState, [event.target.name]: event.target.value})
+    }
+
+    const resetFields = () => {
+        setPersonState({fname: "", lname: ""})
+    }
+
+    const saveFields = () => {
+        console.log(personState.fname+" "+personState.lname);
+        resetFields();
+    }
+
+    return (
+        <>
+            <input type='text' value={personState.fname} onChange={(event) => onChanged(event)} name='fname'/>
+            <input type='text' value={personState.lname} onChange={(event) => onChanged(event)} name='lname'/>
+            <input type='button' value='Reset' onClick={resetFields}/>
+            <input type='button' value='Save' onClick={saveFields}/>
+        </>
+    )
+}
+
+export default Person;
