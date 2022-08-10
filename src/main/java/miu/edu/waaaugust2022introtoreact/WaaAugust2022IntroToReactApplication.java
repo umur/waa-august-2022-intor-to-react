258be05084447c70423297c500ebe69797594074
LaithNassar93 <laith.nassar93@gmail.com>
2022-08-10 18:54:48 -0500

    Commit

---
+++ src/main/java/miu/edu/waaaugust2022introtoreact/WaaAugust2022IntroToReactApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.waaaugust2022introtoreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaAugust2022IntroToReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaaAugust2022IntroToReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miu/edu/waaaugust2022introtoreact/WaaAugust2022IntroToReactApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.waaaugust2022introtoreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaAugust2022IntroToReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
